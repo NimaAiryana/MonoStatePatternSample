8f3605f1754452bb9bc1be04dd76eb719cc10ae6
NimaAiryana <airyana.nima@gmail.com>
2022-01-31 05:59:30 +0330

    init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using MonoStatePatternSample;
+
+Console.WriteLine("Hello, Mono State Pattern :D");
+
+var ceo = new CEO();
+ceo.Name = "Nima Airyana";
+ceo.Age = 28;
+Console.WriteLine(ceo.ToString());
+
+var ceo2 = new CEO();
+Console.WriteLine(ceo2.ToString()); // mono state activated :D
+
+namespace MonoStatePatternSample
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+}
